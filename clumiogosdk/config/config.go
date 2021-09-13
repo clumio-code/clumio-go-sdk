8b5a58ba91d9f743bf7e847c8b0452557ed40d00
jenkins <jenkins@clumio.com>
2021-09-13 13:33:56 +0000

    go SDK changes.

--- clumiogosdk/config/config.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Clumio All Rights Reserved
-
-// Package config has the types required for configuration
-package config
-
-type Config struct {
-	Token   string
-	BaseUrl string
-}
